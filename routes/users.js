88dcfb9855e699564233597f641ee1c1c0360d20
luis2000c <59964726+luis2000c@users.noreply.github.com>
2021-07-02 13:16:04 -0500

    Subida a heroku

---
+++ routes/users.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+const { findUsers } = require("../services/Clients.service")
+const {createUser} = require("../services/Clients.service")
+/* GET users listing. */
+router
+  .get('/', async function (req, res, next) {
+    try {
+      const users = await findUsers()
+      res.status(200).json({
+        msg: "Path Users",
+        body: users
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        msg: "Internal Server Error",
+      })
+    }
+  })
+  .post('/', async (req, res) => {
+    try {
+      let {body : user} = req
+      const result = await createUser(user)
+      res.status(200).json({
+        msg: "usuario creado",
+        body: result.ops,
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        msg: "Internal Server Error",
+      })
+    }
+  });
+
+module.exports = router;
---
+++ services/Clients.service.js
@@ -0,0 +1,35 @@
+
+const { MongoConnection } = require("../lib/Mongo")
+
+
+//Collection
+const COLLECTION = "clients";
+
+const findUsers = () => new Promise(async (resolve, reject) => {
+    try {
+        //Inicializo mongoclient para que me retorne la configuracion de la db
+        const DB = await  MongoConnection()
+        //Obtenemos la colección
+        const clients = DB.collection(COLLECTION)
+        const clientList = await clients.find({}).toArray()
+        resolve(clientList)
+    } catch (error) {
+        reject(error)
+    }
+
+})
+
+const createUser = (user) => new Promise(async(resolve,reject)=>{
+    try {
+        const DB = await MongoConnection()
+        const clients = DB.collection(COLLECTION)
+        const result = await clients.insertOne(user)
+        resolve(result)
+    } catch (error) {
+        reject(error)
+    }
+})
+
+module.exports = {
+    findUsers, createUser,
+}
